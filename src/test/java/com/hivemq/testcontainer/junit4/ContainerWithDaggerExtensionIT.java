02311bca6a1e7f41757f787ab3984a514b4018d6
yweber <yannick.weber@hivemq.com>
2020-04-09 08:34:59 +0200

    added JUnit4 runtime and JUnit4 Dagger IT

---
+++ src/test/java/com/hivemq/testcontainer/junit4/ContainerWithDaggerExtensionIT.java
@@ -0,0 +1,40 @@
+package com.hivemq.testcontainer.junit4;
+
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.testcontainer.core.HiveMQExtension;
+import com.hivemq.testcontainer.util.TestPublishModifiedUtil;
+import com.hivemq.testcontainer.util.dagger.MyExtensionWithDagger;
+import com.hivemq.testcontainer.util.dagger.MyModule;
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Yannick Weber
+ */
+public class ContainerWithDaggerExtensionIT {
+
+    @Rule
+    public final @NotNull HiveMQTestContainerRule rule =
+            new HiveMQTestContainerRule()
+                    .withExtension(HiveMQExtension.builder()
+                            .id("extension-1")
+                            .name("my-extension")
+                            .version("1.0")
+                            .mainClass(MyExtensionWithDagger.class)
+                            .addAdditionalClass(MyModule.class)
+                            .addAdditionalClass(Class.forName("com.hivemq.testcontainer.util.dagger.MyModule_ProvidePublishModifierFactory"))
+                            .build());
+
+    public ContainerWithDaggerExtensionIT() throws ClassNotFoundException {
+    }
+
+    @Test()
+    @Timeout(value = 5, unit = TimeUnit.MINUTES)
+    void test_single_class_extension() throws ExecutionException, InterruptedException {
+        TestPublishModifiedUtil.testPublishModified(rule.getMqttPort());
+    }
+}
