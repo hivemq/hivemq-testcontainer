10eb8c68b5bf5bbca63bc9259b9b6126456eb581
yweber <yannick.weber@hivemq.com>
2020-04-16 09:52:50 +0200

    added license headers removed unnecessary files improved publication structure

--- src/test/java/com/hivemq/testcontainer/junit4/DebuggingIT.java
+++ src/test/java/com/hivemq/testcontainer/junit4/DebuggingIT.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 HiveMQ and the HiveMQ Community
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hivemq.testcontainer.junit4;
 
 import com.hivemq.extension.sdk.api.annotations.NotNull;
@@ -5,11 +20,9 @@ import com.hivemq.testcontainer.core.HiveMQExtension;
 import com.hivemq.testcontainer.util.MyExtension;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.jupiter.api.Timeout;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.concurrent.TimeUnit;
 
 /**
  * This IT is to test the debugging server in the docker container.
