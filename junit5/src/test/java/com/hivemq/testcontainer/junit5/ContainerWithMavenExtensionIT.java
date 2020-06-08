f7538b3fd4671409b02b370edb217ae4f2eac4e8
yweber <yannick.weber@hivemq.com>
2020-06-08 15:42:12 +0200

    maven extension supplier

---
+++ junit5/src/test/java/com/hivemq/testcontainer/junit5/ContainerWithMavenExtensionIT.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 HiveMQ and the HiveMQ Community
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.testcontainer.junit5;
+
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.testcontainer.core.MavenExtensionSupplier;
+import com.hivemq.testcontainer.util.TestPublishModifiedUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Yannick Weber
+ */
+public class ContainerWithMavenExtensionIT {
+
+    @RegisterExtension
+    public final @NotNull HiveMQTestContainerExtension extension =
+            new HiveMQTestContainerExtension()
+                    .withExtensionSupplier(new MavenExtensionSupplier("src/test/resources/maven-extension/pom.xml"));
+
+    @Test()
+    @Timeout(value = 5, unit = TimeUnit.MINUTES)
+    void test_single_class_extension() throws ExecutionException, InterruptedException {
+        TestPublishModifiedUtil.testPublishModified(extension.getMqttPort());
+    }
+
+}
