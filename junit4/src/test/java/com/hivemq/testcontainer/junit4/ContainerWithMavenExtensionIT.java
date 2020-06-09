b21e4700367c7e4a9e3f503067b2cb2920778e0b
yweber <yannick.weber@hivemq.com>
2020-06-09 08:31:15 +0200

    added junit4 tests updated license headers added maven-extensions target folder to .gitignore

---
+++ junit4/src/test/java/com/hivemq/testcontainer/junit4/ContainerWithMavenExtensionIT.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 HiveMQ and the HiveMQ Community
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.testcontainer.junit4;
+
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.testcontainer.core.MavenExtensionSupplier;
+import com.hivemq.testcontainer.util.TestPublishModifiedUtil;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author Yannick Weber
+ */
+public class ContainerWithMavenExtensionIT {
+
+    @Rule
+    public final @NotNull HiveMQTestContainerRule extension =
+            new HiveMQTestContainerRule()
+                    .withExtensionSupplier(new MavenExtensionSupplier("src/test/resources/maven-extension/pom.xml"));
+
+    @Test
+    public void test_single_class_extension() throws ExecutionException, InterruptedException {
+        TestPublishModifiedUtil.testPublishModified(extension.getMqttPort());
+    }
+
+}
