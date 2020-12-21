35607c6df158bf47f86c884eb5890c66d0bb0dfb
yweber <yannick.weber@hivemq.com>
2020-12-21 13:26:09 +0100

    removed unused import

--- core/src/main/java/com/hivemq/testcontainer/core/GradleHiveMQExtensionSupplier.java
+++ core/src/main/java/com/hivemq/testcontainer/core/GradleHiveMQExtensionSupplier.java
@@ -23,6 +23,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.AbstractMap;
 import java.util.Map;
 import java.util.function.Supplier;
