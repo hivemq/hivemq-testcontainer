a428fefceda373342410411cc0068d1f7582273d
Silvio Giebl <silvio.giebl@hivemq.com>
2021-04-09 14:55:44 +0200

    build > migrate gradle to kts

---
+++ junit4/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java-library")
+}
+
+description = "JUnit 4 API for testing HiveMQ Extensions and Java MQTT Applications."
+
+metadata {
+    readableName.set("HiveMQ Testcontainer JUnit4")
+}
+
+dependencies {
+    api(project(":hivemq-testcontainer-core"))
+
+    compileOnly("junit:junit:${property("junit4.version")}")
+    compileOnly("com.hivemq:hivemq-extension-sdk:${property("hivemq-extension-sdk.version")}")
+
+    testImplementation(project(":hivemq-testcontainer-core").dependencyProject.sourceSets.test.get().output)
+    testImplementation("com.hivemq:hivemq-extension-sdk:${property("hivemq-extension-sdk.version")}")
+    testImplementation("com.hivemq:hivemq-mqtt-client:${property("hivemq-mqtt-client.version")}")
+    testImplementation("org.apache.httpcomponents:httpclient:${property("httpclient.version")}")
+    testImplementation("ch.qos.logback:logback-classic:${property("logback.version")}")
+}
