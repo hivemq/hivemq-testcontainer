ec421b24995a05a9ffc6c631f683df1ded5f90b1
Silvio Giebl <silvio.giebl@hivemq.com>
2021-04-13 13:42:10 +0200

    build > added missing group to root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin")
 }
 
+group = "com.hivemq"
 description = "Testcontainers for testing HiveMQ Extensions and Java MQTT Applications."
 
 nexusPublishing {
